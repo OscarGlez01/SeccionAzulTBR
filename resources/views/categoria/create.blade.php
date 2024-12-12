8515ec6086d1837eafb346cfbf1175a4c5ae9d6a
Oscar Gonzalez Leyva <60475217+OscarGlez01@users.noreply.github.com>
2024-12-12 11:56:14 -0700

    Adding forms and CRUD to first entity

--- resources/views/categoria/create.blade.php
+++ resources/views/categoria/create.blade.php
@@ -1,10 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Category') }} : {{__('Create a new register')}}
+            {{ __('Category') }}
         </h2>
     </x-slot>
 
-    
+    <div class="mx-auto my-5 max-w-screen-lg">
+        <h1 class="text-xl text-black dark:text-white ">Añadir un registro</h1>
+        <hr class="my-4 border-gray-800 dark:border-neutral-400">
+        <form action="" class="bg-gray-100 border border-gray-400 rounded px-8 pt-6 pb-8 mb-4 dark:bg-slate-900 dark:border-neutral-400">
+            <div class="mb-4">
+                <label class="form-crud-label" for="nombre">
+                    {{ __('Name')}}
+                </label>
+                <input
+                    class="form-crud-input"
+                    id="nombre" type="text" placeholder="Nombre de la categoría" />
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="nombre">
+                    {{__('Description')}}
+                </label>
+                <input
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:ring"
+                    id="nombre" type="text" placeholder="Describe la categoría" />
+            </div>
+            <div class="mb-2">
+                <button class="btn btn-add">{{__('Create')}}</button>
+            </div>
+        </form>
+    </div>
 
 </x-app-layout>
--- resources/views/categoria/index.blade.php
+++ resources/views/categoria/index.blade.php
@@ -4,24 +4,18 @@
             {{ __('Category') }}
         </h2>
     </x-slot>
-    <div>
-        <a href="{{ route('categorias.create') }}" class="btn btn-info">Registrar</a>
-        <hr>
-        <div class="table-responsive">
-            <table class="table table-condensed table-hover">
-                <thead>
-                    <th>
-                        id
-                    </th>
-                    <th>
-                        Nombre
-                    </th>
-                    <th>
-                        Descripción
-                    </th>
-                    <th>
-                        Action
-                    </th>
+    <div class="mx-auto my-5 max-w-screen-lg">
+        <a href="{{ route('categorias.create') }}" class="btn btn-add">Registrar</a>
+        <hr class="my-4">
+        <div class="">
+            <table class="table-auto w-full text-left rtl:text-right">
+                <thead class="">
+                    <tr>
+                        <th>ID</th>
+                        <th>{{__('Name')}}</th>
+                        <th>{{__('Description')}}</th>
+                        <th>{{__('Actions')}}</th>
+                    </tr>
                 </thead>
                 <tbody>
                     @foreach ($categorias as $categoria)
@@ -30,17 +24,19 @@
                             <td>{{ $categoria->nombre }} </td>
                             <td>{{ $categoria->descripcion }} </td>
                             <td>
-                                <a href="{{ route('destroy.categoria', $categoria->categoria_id) }}" class="btn btn-danger"
-                                    onclick="return confirm('¿Seguro deseas eliminarlo?')"><i
+                                <a href="{{ route('destroy.categoria', $categoria->categoria_id) }}"
+                                    class="btn btn-danger" onclick="return confirm('¿Seguro deseas eliminarlo?')"><i
                                         class="far fa-trash-alt"></i></a>
-                                <a href="{{ route('edit.categoria', $categoria->categoria_id) }}" class="btn btn-warning"><i
-                                        class="far fa-edit"></i></a>
+                                <a href="{{ route('edit.categoria', $categoria->categoria_id) }}"
+                                    class="btn btn-warning"><i class="far fa-edit"></i></a>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
-            {!! $categorias->render() !!}
+        </div>
+        <div class="d-flex justify-content-center">
+            {{ $categorias->links() }}
         </div>
     </div>
 </x-app-layout>
